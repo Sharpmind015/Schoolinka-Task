cc0089997795d54d85aa34d5d312a330dfdda525
Akindeji <akindejioluwaseun015@gmail.com>
2023-09-11 05:52:45 +0100

    chore : customize theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,31 @@ export default {
           one: "#3F5BF6",
           two: "#0E31F2",
         },
+        primary: {
+          50: "#F9F5FF",
+        },
         gray: {
+          50: "#F9FAFB",
+          100: "#F2F4F7",
           200: "#EAECF0",
+          300: "#D0D5DD",
+          500: "#667085",
+          600: "#475467",
+          700: "#344054",
+          800: "#1D2939",
+          900: "#101828",
         },
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
+        workSans: ["Work Sans", "sans-serif"],
+      },
+      screens: {
+        "1xl": "1440px",
+      },
+      boxShadow: {
+        one: "0 1px 2px 0px rgba(16, 24, 40, 0.05)",
+        two: " 0px 8px 8px -4px rgba(16, 24, 40, 0.03), 0px 20px 24px -4px rgba(16, 24, 40, 0.08)",
       },
     },
   },
